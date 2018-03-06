b3163f173c00d07ea5044296f14d5e340bb1f9d6
Cyanococcus Rex <cyanococcusrex@blueberryempire.com>
2018-03-05 20:26:24 -0500

    Made Header.js more dynamic.  Added some characters to font.  Added an 'About' page.

--- Header.js
+++ Header.js
@@ -1,4 +1,11 @@
+
 var d = document;
+
+var scriptHeader = d.getElementById("scriptHeader");
+var scriptHeaderSrc = scriptHeader.getAttribute("src");
+var scriptHeaderSrcSplit = scriptHeaderSrc.split("../");
+scriptHeaderSrcSplit[scriptHeaderSrcSplit.length - 1] = "";
+var rootPathRelative = scriptHeaderSrcSplit.join("../");
 
 var divHeader = d.createElement("div");
 
@@ -6,22 +13,20 @@ var linkHome = d.createElement("a");
 linkHome.href = "index.html";
 linkHome.alt = "Home";
 
-//var imgHome = d.createElement("img");
-//imgHome.src = "Images/BlueberryEmpire.svg";
-//imgHome.width = "20%";
-//linkHome.appendChild(imgHome);
-linkHome.innerHTML = "<img src='Images/BlueberryEmpire.svg' width='33%'></img>";
+linkHome.innerHTML = 
+	"<img src='" + rootPathRelative 
+	+ "Images/BlueberryEmpire.svg' width='33%'></img>";
 
 divHeader.appendChild(linkHome);
 
 var divLinks = d.createElement("div");
-var linkNames = [ "News", "Games", "Contact" ];
+var linkNames = [ "News", "Games", "Feedback", "About" ];
 for (var i = 0; i < linkNames.length; i++)
 {
 	var linkName = linkNames[i];
 
 	var link = d.createElement("a");
-	link.href = linkName + ".html";
+	link.href = rootPathRelative + linkName + ".html";
 	link.innerHTML = linkName;
 	divLinks.appendChild(link);
 	divLinks.innerHTML += "\t";
