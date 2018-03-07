8091c6243eb7e478de9ccbb7c3fff41ff444b488
Cyanococcus Rex <cyanococcusrex@blueberryempire.com>
2018-03-06 21:36:55 -0500

    Added many stub pages for games, with logos where available.

--- Header.js
+++ Header.js
@@ -20,7 +20,7 @@ linkHome.innerHTML =
 divHeader.appendChild(linkHome);
 
 var divLinks = d.createElement("div");
-var linkNames = [ "News", "Games", "Feedback", "About", "Development" ];
+var linkNames = [ "News", "Games", "Feedback", "Development", "About" ];
 for (var i = 0; i < linkNames.length; i++)
 {
 	var linkName = linkNames[i];
